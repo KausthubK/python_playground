23b3d1884ca65d7fffca43ad347e03ee64c80eb0
Kausthub Krishnamurthy (Mission Systems) <kausthub.krishnamurthy@missionsystems.com.au>
2021-03-11 10:44:07 +1100

    adding comments to explain what's going on

--- image_display_loop/stacked.py
+++ image_display_loop/stacked.py
@@ -20,7 +20,7 @@ def main():
     img1 = concatenate_images_h(im_list=images1)
     img2 = concatenate_images_h(im_list=images2)
 
-    cv2.namedWindow("Stacked Image", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("Stacked Image", cv2.WINDOW_NORMAL)  # this bit allows the frame to be resizeable
     for i in range(10):
         if i%2 == 0:
             img_stacked = concatenate_images_v([img1, img2])
@@ -32,7 +32,18 @@ def main():
         cv2.imshow("Stacked Image", np.asarray(img_stacked))
         cv2.waitKey(1000)
 
+
+## Concatenation functions for PIL Images - a similar thing can be done with np.stack if you're using the images as numpy arrays.
 def concatenate_images_h(im_list):
+    """
+    Concatenates images horizontally cornered to the max height and the sum of the widths.
+    
+    Args:
+        im_list (list): list of PIL.Image objects
+    
+    Returns:
+        PIL.Image object
+    """
     total_h = max([im.height for im in im_list])
     total_w = sum([im.width for im in im_list])
     cim = Image.new('RGB', (total_w, total_h))
@@ -41,6 +52,15 @@ def concatenate_images_h(im_list):
     return cim
 
 def concatenate_images_v(im_list):
+    """
+    Concatenates images vertically cornered to the sum of the heights and the max width.
+    
+    Args:
+        im_list (list): list of PIL.Image objects
+    
+    Returns:
+        PIL.Image object
+    """
     total_h = sum([im.height for im in im_list])
     total_w = max([im.width for im in im_list])
     cim = Image.new('RGB', (total_w, total_h))
