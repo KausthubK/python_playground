45c342b9b711568a96cf0ddf5ccaa43d2670db26
Kausthub Krishnamurthy <kausthub.k@live.com>
2022-03-30 21:00:06 +1000

    pytest custom markers

--- loggers/test/logging_test.py
+++ loggers/test/logging_test.py
@@ -1,6 +1,18 @@
 from my_logger import logging as sut
+import pytest
 
 class TestJSONLogger:
+    @pytest.mark.custom
+    def test_json_logger_custom_test(self, caplog):
+        LOG = sut.get_json_logger(name="test-json-logger")
+        LOG.setLevel(level="DEBUG")
+        LOG.info("This is info")
+        assert len(caplog.records) == 1
+        assert caplog.records[0].levelname == "INFO"
+        LOG.debug("This is debugging")
+        assert len(caplog.records) == 2
+        assert caplog.records[1].levelname == "DEBUG"
+
     def test_json_logger(self, caplog):
         LOG = sut.get_json_logger(name="test-json-logger")
         LOG.setLevel(level="DEBUG")
@@ -14,3 +26,4 @@ class TestJSONLogger:
         assert caplog.records[1].levelname == "DEBUG"
         assert caplog.records[2].levelname == "WARNING"
         assert caplog.records[3].levelname == "ERROR"
+
