0efad98d3f9863432d8bb82447586ef673145569
Kausthub Krishnamurthy <kausthub.k@live.com>
2023-08-12 05:17:54 +1000

    refactor: test clean up

--- dataframe_validation/tests/operations/test_grouping_operations.py
+++ dataframe_validation/tests/operations/test_grouping_operations.py
@@ -6,9 +6,12 @@ from dataframe_validation.operations import grouping_operations as sut
 
 
 def test_success_group_by_group_name(good_input_df, good_output_df):
+    print(good_input_df)
     out_df = sut.group_by_group_name(good_input_df)
     pd.testing.assert_frame_equal(left=out_df, right=good_output_df, check_exact=True)
+    print(out_df)
 
-def test_fail_group_by_group_name(bad_input_df, good_output_df):
+def test_fail_group_by_group_name(bad_input_df):
+    print(bad_input_df)
     with pytest.raises(ValidationError):
         out_df = sut.group_by_group_name(bad_input_df)
