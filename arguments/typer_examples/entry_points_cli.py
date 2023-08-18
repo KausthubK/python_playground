144361c9c6a0d11975965fac556b59646176c56c
Kausthub Krishnamurthy <kausthub.k@live.com>
2023-08-18 14:20:03 +1000

    more typer things

--- arguments/typer_examples/entry_points_cli.py
+++ arguments/typer_examples/entry_points_cli.py
@@ -1,21 +1,25 @@
 from typing import Optional
 import typer
 
-app = typer.Typer()
 
-@app.command()
+example_app = typer.Typer()
+
+
+@example_app.command()
 def say_hello(name: Optional[str] = None):
     if name:
         typer.echo(f"Hello {name}")
     else:
         typer.echo("Hello World")
 
-@app.command()
+
+@example_app.command()
 def say_goodbye(name: Optional[str] = None):
     if name:
         typer.echo(f"Goodbye {name}")
     else:
         typer.echo("Goodbye World")
 
+
 if __name__ == "__main__":
-    app()
+    example_app()
